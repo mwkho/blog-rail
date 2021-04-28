11912acdcbd5436015a5c0c3e778d7fc90dee381
mwkho <martin.h@hotmail.ca>
2021-04-28 00:07:31 +0000

    Add delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,12 @@ class ArticlesController < ApplicationController
   end
    
   def edit
+    puts articles_path
     @article = Article.find(params[:id])
   end
    
   def create
     @article = Article.new(article_params)
-   
     if @article.save
       redirect_to @article
     else
@@ -32,8 +32,14 @@ class ArticlesController < ApplicationController
     else 
       render 'edit'
     end
-
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private 
   def article_params
     params.require(:article).permit(:title, :text)
